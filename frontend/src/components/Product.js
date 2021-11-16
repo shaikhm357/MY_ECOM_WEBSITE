1f7148f0db944ba9043b8617d6d1f7f67f036b6c
shaikhm <shaikhm357@gmail.com>
2021-11-16 23:09:03 +0530

    added Rating feature

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Rating from "./Rating";
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3  rounded'>
@@ -17,7 +18,7 @@ const Product = ({ product }) => {
         </Link>
         <Card.Text as='div'>
           <div className='my-3'>
-            {product.rating} from {product.numReviews} reviews
+            <Rating value={product.rating} text={product.numReviews} />
           </div>
         </Card.Text>
         <Card.Text as='h3'>₹{Math.round(product.price * 72)}</Card.Text>
