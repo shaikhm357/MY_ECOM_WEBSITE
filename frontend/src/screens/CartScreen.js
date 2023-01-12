97db58975a11b40cec0e261fc8b1c644f559372a
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-01-12 23:15:57 +0530

    added changes for place order screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -79,7 +79,7 @@ const CartScreen = ({ match, location, history }) => {
               {cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)}
             </ListGroup.Item>
             <ListGroup.Item>
-              <Button type="button" className="btn-block" disabled={cartItems.length === 0} onClick={checkoutHandle}>
+              <Button type="button" className="btn-block w-100" disabled={cartItems.length === 0} onClick={checkoutHandle}>
                 Proceed To Checkout
               </Button>
             </ListGroup.Item>
